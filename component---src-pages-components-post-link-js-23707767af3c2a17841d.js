7b6d699fa4791bfc53928dd75879d618bcddb2d6
liujintao <liujintao@cambricon.com>
2023-11-05 11:25:08 +0800

    diary

---
+++ component---src-pages-components-post-link-js-23707767af3c2a17841d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcabin=self.webpackChunkcabin||[]).push([[271],{9164:function(t,e,l){l.r(e);var n=l(1883),r=l(7294);e.default=t=>{let{post:e}=t;return r.createElement("div",null,r.createElement(n.rU,{to:"."+(null==e?void 0:e.frontmatter.slug)},null==e?void 0:e.frontmatter.date," ",null==e?void 0:e.frontmatter.title))}}}]);
+//# sourceMappingURL=component---src-pages-components-post-link-js-23707767af3c2a17841d.js.map
